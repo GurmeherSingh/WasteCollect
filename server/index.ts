190700fe542837b6fd131e5987eb7f840148ba73
Gurmeher Singh <gurumehersingh1706@gmail.com>
2025-03-18 04:13:12 -0400

    Configured .env successfully

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,13 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use((req, res, next) => {
   const start = Date.now();
   const path = req.path;
@@ -68,3 +70,4 @@ app.use((req, res, next) => {
     log(`serving on port ${port}`);
   });
 })();
+
